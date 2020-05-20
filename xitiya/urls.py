5f7288fda1ef4bc4b8ede9a5fd2f6bd3a3787f11
伍尚滨 <wushangbin@ctrgame.com>
2020-05-20 13:50:04 +0800

    x

--- xitiya/urls.py
+++ xitiya/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myxitiya import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("test/", views.as_views())
 ]
